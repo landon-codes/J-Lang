8ea9e15e1db4c45d3e65aa80fe49f805e00e766a
Landon <230772997+landon-codes@users.noreply.github.com>
2026-04-23 19:15:15 -0500

    Create the Token class

--- lexer.cpp
+++ lexer.cpp
@@ -1,10 +1,27 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
+#include <vector>
+
+using namespace std;
+using std::string;
+using std::vector;
 
 class Token {
-    
+    public:
+        string type;
+        string value;
+
+    Token(string tokenType, string tokenValue) {
+        type = tokenType;
+        value = tokenValue;
+    }
 };
 
-class Lexer {
-    
-};
+int main() {
+    Token test("testType", "testValue");
+
+    cout << test.type << endl << test.value << endl;
+    cout << u8"成功！";
+
+    return 0;
+}
